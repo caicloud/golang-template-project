54d3b4838a62baf6d4867851ce655752a25856ea
Lei Qian <qianlei@caicloud.io>
2020-05-11 17:41:43 +0800

    chore(hack): support setting max cpus manually (#16)

--- hack/read_cpus_available.sh
+++ hack/read_cpus_available.sh
@@ -2,21 +2,23 @@
 
 set -e
 
-CPUS_AVAILABLE=1
+if [ -z "$MAX_CPUS" ]; then
+    MAX_CPUS=1
 
-case "$(uname -s)" in
-Darwin)
-    CPUS_AVAILABLE=$(sysctl -n machdep.cpu.core_count)
-    ;;
-Linux)
-    CFS_QUOTA=$(cat /sys/fs/cgroup/cpu/cpu.cfs_quota_us)
-    if [ $CFS_QUOTA -ge 100000 ]; then
-    CPUS_AVAILABLE=$(expr ${CFS_QUOTA} / 100 / 1000)
-    fi
-    ;;
-*)
-    # Unsupported host OS. Must be Linux or Mac OS X.
-    ;;
-esac
+    case "$(uname -s)" in
+    Darwin)
+        MAX_CPUS=$(sysctl -n machdep.cpu.core_count)
+        ;;
+    Linux)
+        CFS_QUOTA=$(cat /sys/fs/cgroup/cpu/cpu.cfs_quota_us)
+        if [ "$CFS_QUOTA" -ge 100000 ]; then
+            MAX_CPUS=$(("$CFS_QUOTA" / 100 / 1000))
+        fi
+        ;;
+    *)
+        # Unsupported host OS. Must be Linux or Mac OS X.
+        ;;
+    esac
+fi
 
-echo ${CPUS_AVAILABLE}
+echo "$MAX_CPUS"
